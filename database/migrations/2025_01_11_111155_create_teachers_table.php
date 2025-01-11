c9db3d63dcec11d86918e02a3c04dad9e750d931
Agbortoko <arreyagbortoko@gmail.com>
2025-01-11 14:30:09 +0100

    Created migrations and models

---
+++ database/migrations/2025_01_11_111155_create_teachers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('firstname', 255);
+            $table->string('lastname', 255);
+            $table->string('id_card_number', 255)->unique();
+            $table->string('address', 255);
+            $table->enum('gender', ['male', 'female']);
+            $table->enum('marital_status', ['married', 'bachelor', 'divorced']);
+            $table->string('phone_number', 255);
+            $table->date('date_of_birth');
+            $table->string('place_of_birth', 255);
+            $table->string('salutation')->comment('Mr, Mrs, Miss, Dr, Prof, Chief, Engr');;
+            $table->string('preferred_language');
+            $table->longText('academic_diplomas')->nullable();
+            $table->longText('professional_diplomas')->nullable();
+            $table->longText('professional_experience')->nullable();
+            $table->longText('other_relevant_info')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
