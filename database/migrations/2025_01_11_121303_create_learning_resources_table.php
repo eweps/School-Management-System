abb393d5779e8a10cea7267345b6c16b8cc04bb6
eweps <eliaswepngongmiles@gmail.com>
2025-01-11 05:30:12 -0800

    Created migrattions and models

---
+++ database/migrations/2025_01_11_121303_create_learning_resources_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('learning_resources', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('teacher_id')->constrained();
+            $table->foreignId('course_id')->constrained();
+            $table->string('name', 255);
+            $table->text('description')->nullable();
+            $table->longText('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('learning_resources');
+    }
+};
