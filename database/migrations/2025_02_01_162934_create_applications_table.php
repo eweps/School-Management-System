e08736a12830fa865303d682af1a4d564f10b878
Agbortoko <arreyagbortoko@gmail.com>
2025-02-01 17:40:57 +0100

    Added the applications table

---
+++ database/migrations/2025_02_01_162934_create_applications_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname', 255);
+            $table->string('lastname', 255);
+            $table->string('email')->unique();
+            $table->foreignId('course_session_id')->constrained();
+            $table->foreignId('diploma_id')->constrained();
+            $table->foreignId('department_id')->constrained();
+            $table->foreignId('level_id')->constrained();
+            $table->string('id_card_number', 255)->unique();
+            $table->string('address', 255);
+            $table->enum('gender', ['male', 'female']);
+            $table->enum('marital_status', ['married', 'bachelor', 'divorced']);
+            $table->string('phone_number', 255);
+            $table->date('date_of_birth');
+            $table->string('place_of_birth', 255);
+            $table->string('salutation')->comment('Mr, Mrs, Miss, Dr, Prof, Chief, Engr');
+            $table->string('preferred_language');
+            $table->longText('academic_diplomas')->nullable();
+            $table->longText('professional_diplomas')->nullable();
+            $table->longText('professional_experience')->nullable();
+            $table->longText('other_relevant_info')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applications');
+    }
+};
