91d631210468d8caeb290738ca2576c7d01fd553
rabbitmaid <rabbitmaid@proton.me>
2025-05-09 08:26:40 +0100

    Created migration to remove firstname and lastname from applications table

---
+++ database/migrations/2025_05_09_072335_remove_firstname_and_lastname_column_from_applications_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            if(Schema::hasColumns("applications", ["firstname", "lastname"])) {
+                $table->dropColumn('firstname');
+                $table->dropColumn('lastname');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->string('firstname', 255);
+            $table->string('lastname', 255);
+        });
+    }
+};
