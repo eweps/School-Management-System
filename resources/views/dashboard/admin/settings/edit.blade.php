39386a4c11e8603d52e73861a0983e9a56a8f0d8
rabbitmaid <rabbitmaid@proton.me>
2025-05-18 16:44:15 +0100

    Move theme settings to user profile

--- resources/views/dashboard/admin/settings/edit.blade.php
+++ resources/views/dashboard/admin/settings/edit.blade.php
@@ -13,13 +13,6 @@
                     <h1 class="text-base">Dashboard / <a href="{{ route('profile.edit') }}" class="text-secondary">System Settings</a></h1>
             </header>
 
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('dashboard.admin.settings.partials.update-system-theme')
-                </div>
-            </div>
-
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('dashboard.admin.settings.partials.update-system-information-form')
