f4abf9ebd4bbea0c8aa8d8bae25fa4ee37227aac
rabbitmaid <rabbitmaid@proton.me>
2025-08-06 07:24:00 +0100

    Code refactoring

--- resources/views/dashboard/admin/courses/index.blade.php
+++ resources/views/dashboard/admin/courses/index.blade.php
@@ -35,75 +35,83 @@
 
                         @isset($courses)
 
-                            @foreach ($courses as $course)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $course->name }}</td>
-                                    <td>{{ $course->code }}</td>
-                                    <td>{{ $course->credit_value }}</td>
-                                    <td>{{ $course->semester->name }}</td>
-                                    <td>{{ $course->level->name ?? 'none' }}</td>
-                                    <td>
-                                        <div class="flex flex-col md:flex-row justify-center items-center gap-3">
+                        @foreach ($courses as $course)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $course->name }}</td>
+                            <td>{{ $course->code }}</td>
+                            <td>{{ $course->credit_value }}</td>
+                            <td>{{ $course->semester->name }}</td>
+                            <td>{{ $course->level->name ?? 'none' }}</td>
+                            <td>
+                                <div class="flex flex-col md:flex-row justify-center items-center gap-3">
 
-                                            <x-view-modal key="{{ $course->id }}" heading="Course Details" button="More">
-                                                <div class="overflow-x-auto">
-                                                    <table
-                                                        class="min-w-full table-auto border border-gray-600 dark:border-gray-700 rounded-md shadow-sm">
-                                                        <tbody
-                                                            class="divide-y divide-gray-700 text-sm text-gray-700 dark:text-neutral-200">
-                                                            <tr>
-                                                                <td class="font-medium px-4 py-3 w-1/3 uppercase">Name</td>
-                                                                <td class="px-4 py-3">{{ $course->name }}</td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td class="font-medium px-4 py-3 uppercase">Level</td>
-                                                                <td class="px-4 py-3 capitalize">{{ $course->level->name ?? 'none' }}
-                                                                </td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td class="font-medium px-4 py-3 uppercase">Description</td>
-                                                                <td class="px-4 py-3 capitalize">{{ $course->description }}
-                                                                </td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td class="font-medium px-4 py-3 uppercase">Code</td>
-                                                                <td class="px-4 py-3">{{ $course->code }}</td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td class="font-medium px-4 py-3 uppercase">Credit</td>
-                                                                <td class="px-4 py-3 capitalize">
-                                                                    {{ $course->credit_value }}</td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td class="font-medium px-4 py-3 uppercase">Semester</td>
-                                                                <td class="px-4 py-3">{{ $course->semester->name }}</td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td class="font-medium px-4 py-3 uppercase">Created</td>
-                                                                <td class="px-4 py-3">
-                                                                    {{ $course->created_at->toFormattedDayDateString() }}
-                                                                </td>
-                                                            </tr>
-                                                        </tbody>
-                                                    </table>
-                                                </div>
-                                            </x-view-modal>
+                                    <x-view-modal key="{{ $course->id }}" heading="Course Details" button="More">
+                                        <div class="overflow-x-auto">
+                                            <table class="min-w-full table-auto border border-gray-600 dark:border-gray-700 rounded-md shadow-sm">
+                                                <tbody class="divide-y divide-gray-700 text-sm text-gray-700 dark:text-neutral-200">
+                                                    <tr>
+                                                        <td class="font-medium px-4 py-3 w-1/3 uppercase">Name</td>
+                                                        <td class="px-4 py-3">{{ $course->name }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="font-medium px-4 py-3 uppercase">Level</td>
+                                                        <td class="px-4 py-3 capitalize">{{ $course->level->name ?? 'none' }}
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="font-medium px-4 py-3 uppercase">Description</td>
+                                                        <td class="px-4 py-3 capitalize">{{ $course->description }}
+                                                        </td>
+                                                    </tr>
 
-                                            <x-primary-linkbutton href="{{ route('admin.courses.edit', $course->id) }}">
-                                                Edit </x-primary-linkbutton>
+                                                    <td class="font-medium px-4 py-3 uppercase">Teachers</td>
+                                                    <td class="px-4 py-3 capitalize">
+                                                        @foreach ($course->teachers as $teacher)
+                                                            <p>{{ $teacher->user->name }}</p>
+                                                        @endforeach
+                                                        @if(count($course->teachers) === 0)
+                                                            <p>No assigned teacher</p>
+                                                        @endif
+                                                    </td>
 
-                                            <form class="delete-form" action="{{ route('admin.courses.delete') }}"
-                                                method="POST">
-                                                @csrf
-                                                @method('delete')
-                                                <input type="hidden" name="id" value="{{ $course->id }}">
-                                                <x-danger-button> Del</x-danger-button>
-                                            </form>
+                                                    <tr>
+                                                        <td class="font-medium px-4 py-3 uppercase">Code</td>
+                                                        <td class="px-4 py-3">{{ $course->code }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="font-medium px-4 py-3 uppercase">Credit</td>
+                                                        <td class="px-4 py-3 capitalize">
+                                                            {{ $course->credit_value }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="font-medium px-4 py-3 uppercase">Semester</td>
+                                                        <td class="px-4 py-3">{{ $course->semester->name }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="font-medium px-4 py-3 uppercase">Created</td>
+                                                        <td class="px-4 py-3">
+                                                            {{ $course->created_at->toFormattedDayDateString() }}
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
                                         </div>
-                                    </td>
-                                </tr>
-                            @endforeach
+                                    </x-view-modal>
+
+                                    <x-primary-linkbutton href="{{ route('admin.courses.edit', $course->id) }}">
+                                        Edit </x-primary-linkbutton>
+
+                                    <form class="delete-form" action="{{ route('admin.courses.delete') }}" method="POST">
+                                        @csrf
+                                        @method('delete')
+                                        <input type="hidden" name="id" value="{{ $course->id }}">
+                                        <x-danger-button> Del</x-danger-button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
 
                         @endisset
 
