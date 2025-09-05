ac489925b9026863277c91dc36e138b1c1d19531
rabbitmaid <rabbitmaid@proton.me>
2025-09-05 19:27:59 +0100

    modified setting update logic

--- app/Http/Controllers/Dashboard/Admin/SystemSettingController.php
+++ app/Http/Controllers/Dashboard/Admin/SystemSettingController.php
@@ -15,20 +15,6 @@ class SystemSettingController extends Controller
         ]);
     }
 
-    /*  public function update(Request $request) {
-        $settings = Setting::all();
-
-        foreach($settings as $setting) {
-            $request->validate([
-                $setting->name => 'required',
-            ]);
-        }
-
-        dd($request->all());
-        
-       
-        // return back()->with(['status' => 'setting-updated']);
-    } */
 
     public function update(Request $request)
     {
@@ -38,33 +24,34 @@ class SystemSettingController extends Controller
         $messages = [];
 
         foreach ($settings as $setting) {
-            if($setting->type !== 'boolean') {
-                $rules[$setting->name] = 'required';    
+            if ($setting->type === 'text') {
+                $rules[$setting->name] = 'required';
                 $messages["{$setting->name}.required"] = 'This field is required.';
-            }
-            else{
-                $rules[$setting->name] = 'boolean';    
+            } elseif ($setting->type === 'int') {
+                $rules[$setting->name] = 'required|numeric';
+                $messages["{$setting->name}.required"] = 'This field is required.';
+            } elseif ($setting->type === 'boolean') {
+                $rules[$setting->name] = 'boolean';
                 $messages["{$setting->name}.boolean"] = 'This field must be true or false.';
             }
         }
 
         $request->validate($rules, $messages);
 
-        if($setting->type !== 'image') {
-            foreach ($settings as $setting) {
-                 $value = $request->input($setting->name);
-                 Setting::where('name', $setting->name)->update(['value' => $value]);
+        foreach ($settings as $setting) {
+
+            if ($setting->type !== 'image') {
+                $value = $request->input($setting->name);
+                Setting::where('name', $setting->name)->update(['value' => $value]);
+            } else {
+                if ($request->hasFile($setting->name)) {
+                    $file = $request->file($setting->name);
+                    $path = $file->store('settings', 'public');
+                    Setting::where('name', $setting->name)->update(['value' => $path]);
+                }
             }
         }
-        else {
 
-            if ($request->hasFile($setting->name)) {
-                $file = $request->file($setting->name);
-                $path = $file->store('settings', 'public');
-                Setting::where('name', $setting->name)->update(['value' => $path]);
-            }
-
-        }
 
         return back()->with(['status' => 'settings-updated']);
     }
